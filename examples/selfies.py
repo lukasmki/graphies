d2292c90d083b5f525fcfebb83270bc159000440
Lukas <lukasarus@gmail.com>
2026-02-17 21:00:40 -0800

    Remove old examples

---
+++ examples/selfies.py
@@ -0,0 +1,12 @@
+import json
+from pathlib import Path
+
+import metaselfies as msf
+
+root = Path(__file__).parent.resolve()
+
+grammar = msf.Grammar.from_file(path=root / "selfies.json")
+alphabet = grammar.to_alphabet()
+
+output_path = root / "selfies_alphabet.json"
+output_path.write_text(json.dumps(alphabet, indent=4))
