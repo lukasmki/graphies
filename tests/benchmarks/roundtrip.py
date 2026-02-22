457edac55cac1e8ac724bddfda52304fb60d34eb
Lukas <lukasarus@gmail.com>
2026-02-22 13:09:34 -0800

    Add selfies reference benchmark

--- tests/benchmarks/roundtrip.py
+++ tests/benchmarks/roundtrip.py
@@ -1,9 +1,11 @@
 from graphies import decode, encode
+import selfies as sf
 from graphies.grammar import Grammar
 
 GRAMMAR = Grammar.from_file("tests/selfies.json")
 SELFIES = "[C][N][C][C][C][O][C][Ring1][#Branch1][Ring1][Branch1][C][Ring1][#Branch1][C][Ring1][=Branch1][Ring1][Branch1]"
 GRAPH = decode(SELFIES, GRAMMAR)
+SMILES = sf.decoder(SELFIES)
 
 
 def test_roundtrip(benchmark):
@@ -20,3 +22,16 @@ def test_decode(benchmark):
 
 def test_encode(benchmark):
     benchmark(encode, GRAPH, GRAMMAR)
+
+
+def test_decode_selfies(benchmark):
+    benchmark(sf.decoder, SELFIES)
+
+def test_encode_selfies(benchmark):
+    benchmark(sf.encoder, SMILES)
+
+def test_roundtrip_selfies(benchmark):
+    def decode_encode(selfies: str):
+        smiles = sf.decoder(selfies)
+        selfies = sf.encoder(smiles)
+    benchmark(decode_encode, SELFIES)
