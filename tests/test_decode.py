260d68b409ee7051f253362a5186f1ad21dced23
Lukas <lukasarus@gmail.com>
2026-03-01 16:28:42 -0800

    Add error handling test cases

--- tests/test_decode.py
+++ tests/test_decode.py
@@ -1,6 +1,6 @@
 import selfies as sf
 
-from graphies import decode
+from graphies import decode, encode
 
 
 def test_ring():
@@ -62,3 +62,58 @@ def test_ring_in_nestedbranch():
     ]
     for u, v, d in bonds:
         assert d["weight"] == graph.edges[u, v]["weight"]
+
+
+def test_case1():
+    """Tests link resolution
+    The first link creates an invalid link target
+    The second link creates a link between C and O (index 0 and 3)
+    The third link would exceed the degree of O and is passed.
+
+    NOTE: selfies gives this: [C][C][C][O][Ring1][Ring2]
+    I'm not sure why selfies decodes this into a ring between
+    the [O] and index 0 [C]. A [Ring1][C] sequence should create a link
+    between the previous atom and index = m - ([C] + 1) atom before it.
+    Unless selfies has a special rule for this, it isn't consistent with
+    the ring indexing.
+    """
+
+    selfies = "[C][C][C][O][Ring2][N][O][Ring1][C][Ring1][C]"
+    graph = decode(selfies, grammar="tests/selfies.json")
+    graphies = encode(graph, grammar="tests/selfies.json")
+    reference = "[C][C][C][=O]"
+    assert graphies == reference
+
+
+def test_case2():
+    """Link additivity
+    The link [Ring2][C][C] creates a link between 5 and 4
+    A single bond already exists so it should add the weight of the link
+    and resolve to a weight=2 bond
+    """
+    selfies = "[O][C][C][C][C][C][Ring2][C][C][O][C]"
+    reference = "[O][C][C][C][C][=C][O][C]"
+    graph = decode(selfies, grammar="tests/selfies.json")
+    graphies = encode(graph, grammar="tests/selfies.json")
+    assert graphies == reference
+
+
+def test_case3():
+    """Node level edge weight reduction
+    Each of the trailing [=O] tokens should get their edge weight reduced
+    """
+    selfies = "[C][C][C][C][C][C][O][O][=O][=O][=O]"
+    reference = "[C][C][C][C][C][C][O][O][O][O][O]"
+    graph = decode(selfies, grammar="tests/selfies.json")
+    graphies = encode(graph, grammar="tests/selfies.json")
+    assert graphies == reference
+
+def test_case4():
+    """Node level max degree reached
+    After the [#N], the remaining degree is maxed out.
+    """
+    selfies = "[C][C][O][C][C][O][C][C][O][C][#N][C][O][C][=Branch2]"
+    reference = "[C][C][O][C][C][O][C][C][O][C][#N]"
+    graph = decode(selfies, grammar="tests/selfies.json")
+    graphies = encode(graph, grammar="tests/selfies.json")
+    assert graphies == reference
