52e4dcd3e3870feb53eb2a703e63c64db2161281
Lukas <lukasarus@gmail.com>
2026-03-03 00:38:58 -0800

    Fix encoding issue for nodes not in token index order

--- src/graphies/encoder.py
+++ src/graphies/encoder.py
@@ -19,17 +19,26 @@ logger = logging.getLogger(__name__)
 class Encoder:
     def __init__(self, grammar: Grammar):
         self.grammar: Grammar = grammar
+        self.visited: list[int] = list()
 
-    def encode(self, graph: Graph) -> str:
+    def encode(self, graph: Graph, source=0) -> str:
         # walk and build token sequence
-        tree = nx.dfs_tree(graph, source=0, sort_neighbors=sorted)
-        tokens = self.walk(graph, tree, node_id=0)
+        tree = nx.dfs_tree(graph, source=source, sort_neighbors=sorted)
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug("STARTING WALK")
+        self.visited = list()
+        tokens = self.walk(graph, tree, node_id=source)
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug("FINISHED WALK")
         return "".join([t.symbol for t in tokens])
 
     def walk(
         self, graph: Graph, tree: DiGraph, node_id: int, parent: int | None = None
     ) -> list[TokenInstance]:
         tokens: list[TokenInstance] = []
+        self.visited.append(node_id)  # add to visited nodes
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug(f"Reached node {node_id} from {parent}")
 
         # add node
         node = NodeInstance(**graph.nodes[node_id])
@@ -40,6 +49,8 @@ class Encoder:
         token = TokenInstance(
             type=TokenType.NODE, node=node, edge=edge, modifiers=node.modifiers
         )
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug(f"Added node {token.serialize()}")
         tokens.append(token)
 
         # get non-tree edges to current node
@@ -56,15 +67,24 @@ class Encoder:
         for child in children[:-1]:
             # branch
             branch_tokens: list[TokenInstance] = self.walk(graph, tree, child, node_id)
-            edge = EdgeInstance(**graph.get_edge_data(node_id, child))
-            branch: BranchInstance = self.grammar.get_branch(size=len(branch_tokens))
-            branch_prefix = [
-                TokenInstance(type=TokenType.BRANCH, node=branch, edge=edge)
-            ]
-            for index in branch.indices:
-                branch_prefix.append(TokenInstance(type=TokenType.INDEX, node=index))
 
-            tokens.extend(branch_prefix + branch_tokens)
+            # create branch prefix
+            edge = EdgeInstance(**graph.get_edge_data(node_id, child))
+            branch_instance: BranchInstance = self.grammar.get_branch(
+                size=len(branch_tokens)
+            )
+            branch_prefix = [
+                TokenInstance(type=TokenType.BRANCH, node=branch_instance, edge=edge)
+            ]
+            for index in branch_instance.indices:
+                branch_prefix.append(TokenInstance(type=TokenType.INDEX, node=index))
+            branch = branch_prefix + branch_tokens
+            tokens.extend(branch)
+
+            if logger.isEnabledFor(logging.DEBUG):
+                logger.debug(
+                    f"Created branch {[token.serialize() for token in branch_prefix]}"
+                )
 
         # create links
         tokens.extend(self.create_link(graph, node_id, links))
@@ -75,11 +95,15 @@ class Encoder:
         return tokens
 
     def create_link(self, graph, node_id, links):
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug(f"Attempting to create link from {node_id} to {links}")
+
         tokens = []
         for link_id in links:
-            if link_id < node_id:
+            if link_id in self.visited:
                 edge = EdgeInstance(**graph.get_edge_data(node_id, link_id))
-                link: LinkInstance = self.grammar.get_link(distance=node_id - link_id)
+                distance = self.visited.index(node_id) - self.visited.index(link_id)
+                link: LinkInstance = self.grammar.get_link(distance)
                 tokens.append(TokenInstance(type=TokenType.LINK, node=link, edge=edge))
                 tokens.extend(
                     [
@@ -87,4 +111,6 @@ class Encoder:
                         for index in link.indices
                     ]
                 )
+        if logger.isEnabledFor(logging.DEBUG) and tokens:
+            logger.debug(f"Added node {[token.serialize() for token in tokens]}")
         return tokens
--- tests/test_encode.py
+++ tests/test_encode.py
@@ -1,6 +1,10 @@
+import random
+
 import networkx as nx
 
-from graphies import encode
+from graphies import Encoder, Grammar, encode
+
+GRAMMAR = Grammar.from_file("tests/selfies.json")
 
 
 def test_process():
@@ -25,10 +29,11 @@ def test_process():
             (5, 0, {"symbol": "-", "weight": 1}),
         ]
     )
-    _ = encode(graph, grammar="tests/selfies.json")
+    graphies = encode(graph, grammar="tests/selfies.json")
+    assert graphies == "[C][=C][-C][=C][-C][=C][-Ring1][=Branch1]"
 
 
-def test_process_edges():
+def test_source():
     graph = nx.Graph()
     graph.add_nodes_from(
         [
@@ -42,18 +47,107 @@ def test_process_edges():
     )
     graph.add_edges_from(
         [
-            # both symbol and weight (symbol prioritized)
             (0, 1, {"symbol": "=", "weight": 2}),
-            (1, 2, {"symbol": "-", "weight": 2}),
-            # only symbol
-            (2, 3, {"symbol": "="}),
-            (3, 4, {"symbol": "-"}),
-            # only weight
-            (4, 5, {"weight": 2}),
-            (5, 0, {"weight": 1}),
+            (1, 2, {"symbol": "-", "weight": 1}),
+            (2, 3, {"symbol": "=", "weight": 2}),
+            (3, 4, {"symbol": "-", "weight": 1}),
+            (4, 5, {"symbol": "=", "weight": 2}),
+            (5, 0, {"symbol": "-", "weight": 1}),
         ]
     )
-    _ = encode(graph, grammar="tests/selfies.json")
+    encoder = Encoder(GRAMMAR)
+    graphies = encoder.encode(graph, source=1)
+    assert graphies == "[C][=C][-C][=C][-C][=C][-Ring1][=Branch1]"
+
+
+def test_randomized():
+    graph = nx.Graph()
+    graph.add_nodes_from(
+        [
+            (0, {"symbol": "C", "degree": 4, "modifiers": []}),
+            (1, {"symbol": "C", "degree": 4, "modifiers": []}),
+            (2, {"symbol": "C", "degree": 4, "modifiers": []}),
+            (3, {"symbol": "C", "degree": 4, "modifiers": []}),
+            (4, {"symbol": "C", "degree": 4, "modifiers": []}),
+            (5, {"symbol": "C", "degree": 4, "modifiers": []}),
+        ]
+    )
+    graph.add_edges_from(
+        [
+            (0, 1, {"symbol": "=", "weight": 2}),
+            (1, 2, {"symbol": "-", "weight": 1}),
+            (2, 3, {"symbol": "=", "weight": 2}),
+            (3, 4, {"symbol": "-", "weight": 1}),
+            (4, 5, {"symbol": "=", "weight": 2}),
+            (5, 0, {"symbol": "-", "weight": 1}),
+        ]
+    )
+    nodeids = list(graph.nodes)
+    random.shuffle(nodeids)
+    graph = nx.relabel_nodes(graph, {n: nodeids[i] for i, n in enumerate(graph.nodes)})
+
+    encoder = Encoder(GRAMMAR)
+    graphies = encoder.encode(graph, source=0)
+    assert (graphies == "[C][=C][-C][=C][-C][=C][-Ring1][=Branch1]") or (
+        graphies == "[C][-C][=C][-C][=C][-C][=Ring1][=Branch1]"
+    )
+
+
+def test_named():
+    graph = nx.Graph()
+    graph.add_nodes_from(
+        [
+            ("A", {"symbol": "C", "degree": 4, "modifiers": []}),
+            ("B", {"symbol": "C", "degree": 4, "modifiers": []}),
+            ("C", {"symbol": "C", "degree": 4, "modifiers": []}),
+            ("D", {"symbol": "C", "degree": 4, "modifiers": []}),
+            ("E", {"symbol": "C", "degree": 4, "modifiers": []}),
+            ("F", {"symbol": "C", "degree": 4, "modifiers": []}),
+        ]
+    )
+    graph.add_edges_from(
+        [
+            ("A", "B", {"symbol": "=", "weight": 2}),
+            ("B", "C", {"symbol": "-", "weight": 1}),
+            ("C", "D", {"symbol": "=", "weight": 2}),
+            ("D", "E", {"symbol": "-", "weight": 1}),
+            ("E", "F", {"symbol": "=", "weight": 2}),
+            ("F", "A", {"symbol": "-", "weight": 1}),
+        ]
+    )
+    encoder = Encoder(GRAMMAR)
+    graphies = encoder.encode(graph, source="A")
+    assert (graphies == "[C][=C][-C][=C][-C][=C][-Ring1][=Branch1]") or (
+        graphies == "[C][-C][=C][-C][=C][-C][=Ring1][=Branch1]"
+    )
+
+
+# def test_process_edges():
+#     graph = nx.Graph()
+#     graph.add_nodes_from(
+#         [
+#             (0, {"symbol": "C", "degree": 4, "modifiers": []}),
+#             (1, {"symbol": "C", "degree": 4, "modifiers": []}),
+#             (2, {"symbol": "C", "degree": 4, "modifiers": []}),
+#             (3, {"symbol": "C", "degree": 4, "modifiers": []}),
+#             (4, {"symbol": "C", "degree": 4, "modifiers": []}),
+#             (5, {"symbol": "C", "degree": 4, "modifiers": []}),
+#         ]
+#     )
+#     graph.add_edges_from(
+#         [
+#             # both symbol and weight (symbol prioritized)
+#             (0, 1, {"symbol": "=", "weight": 2}),
+#             (1, 2, {"symbol": "-", "weight": 2}),
+#             # only symbol
+#             (2, 3, {"symbol": "="}),
+#             (3, 4, {"symbol": "-"}),
+#             # only weight
+#             (4, 5, {"weight": 2}),
+#             (5, 0, {"weight": 1}),
+#         ]
+#     )
+#     _ = encode(graph, grammar="tests/selfies.json")
 
 
 def test_process_nodes():
@@ -61,11 +155,11 @@ def test_process_nodes():
     graph.add_nodes_from(
         [
             (0, {"symbol": "C", "degree": 4}),
-            (1, {"symbol": "C", "degree": 4, "modifiers": []}),
-            (2, {"symbol": "C", "degree": 4, "modifiers": []}),
-            (3, {"symbol": "C", "degree": 4, "modifiers": []}),
-            (4, {"symbol": "C", "degree": 4, "modifiers": []}),
-            (5, {"symbol": "C", "degree": 4, "modifiers": []}),
+            (1, {"symbol": "C", "degree": 4}),
+            (2, {"symbol": "C", "degree": 4}),
+            (3, {"symbol": "C", "degree": 4}),
+            (4, {"symbol": "C", "degree": 4}),
+            (5, {"symbol": "C", "degree": 4}),
         ]
     )
     graph.add_edges_from(
