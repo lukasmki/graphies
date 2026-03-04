ae328fe4f2de3cbfd41f4051a1d45805a38ccb09
Lukas <lukasarus@gmail.com>
2026-03-04 00:00:55 -0800

    Add randomized dataset

--- src/graphies/predict/__init__.py
+++ src/graphies/predict/__init__.py
@@ -3,13 +3,14 @@ from importlib.util import find_spec
 if find_spec("torch") is None:
     raise ImportError("Install torch to use the graphies.predict module")
 
-from .dataset import CSVGraphiesDataset, HFGraphiesDataset
+from .dataset import CSVGraphiesDataset, CSVRandomizedGraphiesDataset, HFGraphiesDataset
 from .inference import GraphiesModel
 from .tokenizer import GraphiesTokenizer
 from .trainer import GraphiesTrainer
 
 __all__ = [
     "CSVGraphiesDataset",
+    "CSVRandomizedGraphiesDataset",
     "HFGraphiesDataset",
     "GraphiesModel",
     "GraphiesTokenizer",
