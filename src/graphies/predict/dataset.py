b76b8940c64a8ff67059033720a407d36878b908
Lukas <lukasarus@gmail.com>
2026-03-03 23:58:40 -0800

    Add dataset for randomized reencoding of graphies

--- src/graphies/predict/dataset.py
+++ src/graphies/predict/dataset.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 
 import polars as pl
@@ -6,6 +7,7 @@ from datasets import Dataset as HFDataset
 from polars import DataFrame
 from torch.utils.data import Dataset
 
+from graphies import Decoder, Encoder
 from graphies.predict.tokenizer import GraphiesTokenizer
 
 
@@ -58,4 +60,37 @@ class CSVGraphiesDataset(Dataset):
         if self.max_length:
             tokens = tokens[: self.max_length]
 
+        return torch.tensor(tokens, dtype=torch.long)
+
+
+class CSVRandomizedGraphiesDataset(Dataset):
+    def __init__(
+        self,
+        path: str | Path,
+        column: str,
+        tokenizer: GraphiesTokenizer,
+        max_length: int | None = None,
+    ):
+        if isinstance(path, str):
+            path = Path(path)
+        path = path.resolve()
+        self.dataset: DataFrame = pl.read_csv(path, columns=[column])
+        self.column: str = column
+        self.tokenizer: GraphiesTokenizer = tokenizer
+        self.max_length: int | None = max_length
+
+        self.encoder = Encoder(self.tokenizer.grammar)
+        self.decoder = Decoder(self.tokenizer.grammar)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index: int):
+        in_graphies = self.dataset.row(index, named=True)[self.column]
+        graph = self.decoder.decode(in_graphies)
+        source = random.choice(list(graph.nodes))
+        graphies = self.encoder.encode(graph, source)
+        tokens = self.tokenizer.encode("[BEGIN]" + graphies + "[END]")
+        if self.max_length:
+            tokens = tokens[: self.max_length]
         return torch.tensor(tokens, dtype=torch.long)
